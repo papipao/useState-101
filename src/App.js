8830de082777f329eb4711a7c194cecf541c0854
Paolo <paolomiguel108@gmail.com>
2021-08-20 15:36:50 +0800

    Fourth Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+  const [text, setText] = React.useState("Hello World!");
+  const changeText = () => {
+    if (text === "Hello World!") {
+      setText("Hi Earth!");
+    } else {
+      setText("Hello World!");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{text}</h1>
+      <div className="wrap">
+        <button type="button" className="btn" onClick={changeText}>
+          Change Text
+        </button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
